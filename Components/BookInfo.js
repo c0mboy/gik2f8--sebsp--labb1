d6770f9a47d5f5fbb44b9ab88bef92e243d03763
c0mboy <90767788+c0mboy@users.noreply.github.com>
2022-11-24 11:27:15 +0100

    Changing to mouseon addeventlistern

--- Components/BookInfo.js
+++ Components/BookInfo.js
@@ -6,8 +6,8 @@ const BookInfo = (book) => {
                 <div class="">
                 <img src=    "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstephenking.com%2Fpromo%2Fdark-tower-2016-collection%2Fimages%2Fthe_gunslinger.jpg"/>
                 <div class="">
-                    <span class="">Author: ${book.title} </span>
-                    <span class="">Title: ${book.author}</span>
+                    <span class="">Author: ${book.author} </span>
+                    <span class="">Title: ${book.title}</span>
                     <span class="">Pages: ${book.pages}</span>
                     <span class="">Release Date: ${book.releaseDate}</span
                 </div> 
@@ -18,3 +18,30 @@ const BookInfo = (book) => {
         return html;
   };
   
+const BookInfoTest = (book) => {
+    let html = `
+        <div class="flex justify-center book-info">
+        <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg bg-gradient-to-tr from-sky-300 to-fuchsia-500">
+            <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src= "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fstephenking.com%2Fpromo%2Fdark-tower-2016-collection%2Fimages%2Fthe_gunslinger.jpg"/ alt="" />
+            <div class="p-6 flex flex-col justify-start">
+            <h5 class="text-gray-900 text-xl font-medium mb-2">Title: ${book.title}</h5>
+            <p class="text-gray-700 text-base mb-4">
+            Author: ${book.author}</p>
+            <p class="text-gray-700 text-xs">Release Pages: ${book.pages}</p>
+            <p class="text-gray-600 text-xs">Release Date: ${book.releaseDate}</p>
+            </div>
+        </div>
+        </div>`;
+       return html;
+};
+
+const BookInfoTestLu = (book) => {
+    let html = 
+    `<li
+    class="book-list__item mb-2 mx-2 last:mb-0 p-3 
+    text-indigo-900 last:border-b-0 border-b 
+    border-indigo-700 cursor-pointer">
+  ${book.author} - ${book.title}    
+  </li>`;
+       return html;
+};
--- script.js
+++ script.js
@@ -22,16 +22,46 @@ searchField.addEventListener('keyup', (e) =>
 
 function renderBookList(bookList) {
   const existingElement = document.querySelector('.book-list');
-
   const root = document.getElementById('root');
 
   existingElement && root.removeChild(existingElement);
   bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', BookList(bookList));
-}
+  
+  
+  document.addEventListener("mouseover", myFunction);
+  
+  function myFunction() {
+    console.log("heheheh")
+    document.getElementById('pointer').innerHTML = "Moused over!"
+    //const insert = document.getElementById('insert');
+  }
 
-function renderBookItem(bookList){
-  console.log("Inserting ")
-  const insert = document.getElementById('insert');
-  let minhtml = BookInfo(bookList)
-  insert.insertAdjacentHTML('afterbegin', minhtml)
+  
 }
+// document.querySelector('.pointer').addEventListener("mouseover", mouseOver);
+// document.querySelector('.pointer').addEventListener("mouseout", mouseOut);
+
+// function mouseOver() {
+  //   document.getElementById("demo").style.color = "red";
+  //   console.log("hey")
+  // }
+  
+  // function mouseOut() {
+    //   document.getElementById("demo").style.color = "black";
+    //   console.log("då")
+    // }
+    
+    
+// function renderBookItem(book){
+//   console.log("hej", book)
+//   const existingElement = document.querySelector('.book-info');
+//   const insert = document.getElementById('insert');
+  
+//   existingElement && insert.removeChild(existingElement);
+
+//   // let minhtml = BookInfoTest(book)
+//   // insert.insertAdjacentHTML('afterbegin', minhtml)
+// }
+
+
+
